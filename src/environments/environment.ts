1bad47e5220f8072f938150acc6c45a121069cab
Cristian Carballo <cristian.carballo1994@gmail.com>
2023-01-19 19:29:24 -0300

    primer comm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseURL: 'https://63c1b39799c0a15d28f06c48.mockapi.io/'
 };
 
 /*
